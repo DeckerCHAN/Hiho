784648aabe8dbdc1d7b8d363deb423afa18e98e2
DeckerCHAN <DeckerCHAN@gmail.com>
2015-05-17 23:58:04 +0800

    First AC a+b

---
+++ APlusB/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace APlusB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String line;
+            while (!String.IsNullOrEmpty(line = Console.ReadLine()))
+            {
+                var a = Convert.ToInt64(line.Split(' ')[0]);
+                var b = Convert.ToInt64(line.Split(' ')[1]);
+                Console.WriteLine(a + b);
+            }
+        }
+    }
+}
